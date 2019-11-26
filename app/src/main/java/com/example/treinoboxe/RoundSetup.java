083fe9670fc032b4c4ce4141cbcfc4b0ba46637a
mbj-junior <mbj_junior@hotmail.com>
2019-11-25 23:00:02 -0300

    rascunho de telas

---
+++ app/src/main/java/com/example/treinoboxe/RoundSetup.java
@@ -0,0 +1,14 @@
+package com.example.treinoboxe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RoundSetup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_round_setup);
+    }
+}
---
+++ app/src/main/java/com/example/treinoboxe/TrainingSetup.java
@@ -0,0 +1,14 @@
+package com.example.treinoboxe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TrainingSetup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_training_setup);
+    }
+}
